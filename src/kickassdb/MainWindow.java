07334d5cd2e7d519d05b6a12fd76235e28d82ec3
Jaime Andres <Jaime Andres@140.114.6.28>
2014-03-27 15:02:38 +0800

    More code organizations

--- src/kickassdb/MainWindow.java
+++ src/kickassdb/MainWindow.java
@@ -55,8 +55,7 @@ public class MainWindow extends javax.swing.JFrame {
         tln.setForeground(Color.LIGHT_GRAY);
         tln.setCurrentLineForeground(Color.WHITE);
         this.queryScrollPane.setRowHeaderView(tln);
-        
-        // HOLA
+                
     }//End MainWindow()
 
     private void printDatabase(){
