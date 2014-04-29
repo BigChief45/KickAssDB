799106309e491015dc877fc1d737fb29a6b0ca33
ocampos16 <oc_campos16@hotmail.com>
2014-04-30 03:18:34 +0800

    * WHERE with aliases Part1, 1 filter works perfectly for * operator

--- src/queries/query2.sql
+++ src/queries/query2.sql
@@ -1,2 +1,2 @@
-SELECT person_id, Person.person_name FROM Person, Vehicle
-WHERE person_id = 1
+SELECT person_id, Person.person_name FROM Person AS P, Vehicle AS V
+WHERE P.vehicle_id = V.vehicle_id
